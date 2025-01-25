cc85a7e2e157f6c8d53ac43ed2285c47f0c2b62d
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-01-25 18:46:19 +0530

    Add files via upload

---
+++ Asteroid_Collision_735.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ vector<int> asteroidCollision(vector<int>& a) {
+        vector<int> s; // use vector to simulate stack.
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] > 0 || s.empty() || s.back() < 0) // a[i] is positive star or a[i] is negative star and there is no positive on stack
+                s.push_back(a[i]);
+            else if (s.back() <= -a[i]) { // a[i] is negative star and stack top is positive star
+                if(s.back() < -a[i]) i--; // only positive star on stack top get destroyed, stay on i to check more on stack.
+                s.pop_back(); // destroy positive star on the frontier;
+            } // else : positive on stack bigger, negative star destroyed.
+        }
+        return s;
+    }
+
+int main(){
+
+    vector<int> a = {5, 10, -5};
+    vector<int> res = asteroidCollision(a);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
