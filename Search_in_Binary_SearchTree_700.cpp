99545d511e74c97c4aba581489a04202cb649a0b
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-02-10 10:07:46 +0530

    Add files via upload

---
+++ Search_in_Binary_SearchTree_700.cpp
@@ -0,0 +1,5 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(!root || root->val == val) return root;
+    else if(root->val > val) return searchBST(root->left, val);
+    else return searchBST(root->right, val);
+}
