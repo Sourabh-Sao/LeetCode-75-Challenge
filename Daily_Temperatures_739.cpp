11f4410952d72a18c2851ac66f63397afa9d63c0
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2025-03-15 09:27:23 +0530

    Add files via upload

---
+++ Daily_Temperatures_739.cpp
@@ -0,0 +1,14 @@
+vector<int> dailyTemperatures(vector<int>& temps) {
+    vector<int> results(temps.size());
+    stack<int> stack;
+    /// UPVOTE !
+    for (int i = 0; i < temps.size(); i++) {
+        while (!stack.empty() && temps[stack.top()] < temps[i]) {
+            results[stack.top()] = i - stack.top();
+            stack.pop();
+        }
+        stack.push(i);
+    }
+
+    return results;
+}
